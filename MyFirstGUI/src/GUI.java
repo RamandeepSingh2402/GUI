0d806309af1f79fa4d0bd8944f3d488b5ced986d
RamandeepSingh2402 <singhramandeep2402@gmail.com>
2022-05-18 15:03:03 -0300

    commented the whole code and finished the front end part of the  login GUI successfully!

--- MyFirstGUI/src/GUI.java
+++ MyFirstGUI/src/GUI.java
@@ -13,44 +13,57 @@ public class GUI implements ActionListener {
     private static JLabel success;
     public static void main(String[] args) {
 
+        //creating an object panel
         panel = new JPanel();
 
+        //creating a new frame
         frame = new JFrame();
+
+        //setting the size of the frame, defaultCloseOperation and adding the panel to the frame
         frame.setSize(400,400);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.add(panel);
 
+        //setting the layout of the panel to be null
         panel.setLayout(null);
 
+        //creating and instantiating a JLabel userLabel, setting its text, bounds and adding it to the panel
         userLabel = new JLabel();
         userLabel.setText("User");
         userLabel.setBounds(10, 20, 80, 25);
         panel.add(userLabel);
 
+        //creating and instantiating a JTextField which takes in the username, setting its bounds and adding it to the panel
         userText = new JTextField(20);
         userText.setBounds(100,20,165,25);
         panel.add(userText);
 
+        //creating and instantiating a JLabel passwordLabel, setting its text, bounds and adding it to the panel
         passwordLabel = new JLabel("Password");
         passwordLabel.setBounds(10,60,80,25);
         panel.add(passwordLabel);
 
+        //creating and instantiating a JPasswordField which takes in password, setting its bounds and adding it to the panel
         passwordText = new JPasswordField(20);
         passwordText.setBounds(100,60,165,25);
         panel.add(passwordText);
 
+        //creating and instantiating a JButton, setting its bounds and adding actionListener and adding it to the panel
         button = new JButton("Login");
         button.setBounds(80,110,80,25);
         button.addActionListener(new GUI());
         panel.add(button);
 
-
+        //creating and instantiating a JLabel, setting its bounds and adding it to the panel
         success = new JLabel("");
         success.setBounds(10,140,300,25);
         panel.add(success);
+
+        //setting the visibility of the frame to be true
         frame.setVisible(true);
     }
 
+    //ActionListener which prints whether the login is successful or not
     @Override
     public void actionPerformed(ActionEvent e) {
         String user = userText.getText();
@@ -59,7 +72,7 @@ public class GUI implements ActionListener {
             success.setText("Login successful!");
         }
         else{
-            success.setText("");
+            success.setText("Login unsuccessful");
         }
     }
 }
