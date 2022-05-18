762d3efdaea017f72689e63d50b3bed00c8ba72e
RamandeepSingh2402 <singhramandeep2402@gmail.com>
2022-05-18 14:24:23 -0300

    created the user label text and the user textField for the user to enter the username and added the same to the panel

--- MyFirstGUI/src/GUI.java
+++ MyFirstGUI/src/GUI.java
@@ -2,12 +2,25 @@ import javax.swing.*;
 
 public class GUI {
     public static void main(String[] args) {
+
         JPanel panel = new JPanel();
+
         JFrame frame = new JFrame();
         frame.setSize(400,400);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setVisible(true);
         frame.add(panel);
+
         panel.setLayout(null);
+
+        JLabel userLabel = new JLabel();
+        userLabel.setText("User");
+        userLabel.setBounds(10, 20, 80, 25);
+        panel.add(userLabel);
+
+        JTextField userText = new JTextField(20);
+        userText.setBounds(100,20,165,25);
+        panel.add(userText);
+
+        frame.setVisible(true);
     }
 }
